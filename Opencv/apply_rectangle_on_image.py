b174520aaac6a75ec2364dc8786bc4699a33015c
Krishna <gundakrishna2004@gmail.com>
2026-04-09 21:10:44 +0530

    Update apply_rectangle_on_image.py

--- Opencv/apply_rectangle_on_image.py
+++ Opencv/apply_rectangle_on_image.py
@@ -4,6 +4,7 @@ import numpy as np
 image = cv2.imread('../Pictures/cameraman.tif', 0)
 
 cv2.rectangle(image, (50, 50), (300, 500), (255, 0, 0), 2) # drawing rectangle on the image
+cv2.line(image,(5,256),(500,256),(0,0,255),2,cv2.LINE_8) # drawing line on the image
 
 cv2.imshow('image', image)
 cv2.waitKey(0)
